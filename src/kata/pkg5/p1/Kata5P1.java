cefaf804663d150d96d134da904195ef1581fad5
usuario <usuario@10.22.148.53>
2017-11-10 09:21:10 +0000

    Kata5P1: commit 1.

---
+++ src/kata/pkg5/p1/Kata5P1.java
@@ -0,0 +1,20 @@
+package kata.pkg5.p1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Kata5P1 {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        Class.forName("org.sqlite.JDBC");
+        Connection c= DriverManager.getConnection("jdbc:sqlite:qwerty.db");
+        Statement st= c.createStatement();
+        ResultSet rs= st.executeQuery("select * from People");
+        while(rs.next()){
+            System.out.println("ID = " + rs.getInt("Id"));
+            System.out.println("Name = " + rs.getString("Name"));
+        }
+    }
+}
